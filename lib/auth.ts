40f20b265e2a5dbb2ef14570065107d89ff9f052
maximiloi <maximiloi@gmail.com>
2026-02-13 22:40:23 +0300

    feat: add function getSession

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,8 @@
-import prisma from '@/lib/prisma';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { headers } from 'next/headers';
+
+import prisma from '@/lib/prisma';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,3 +12,11 @@ export const auth = betterAuth({
     enabled: true,
   },
 });
+
+export async function getSession() {
+  const result = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  return result;
+}
