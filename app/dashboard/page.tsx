1c2be14f2791554db97c61bb4524668f48970b89
maximiloi <maximiloi@gmail.com>
2026-02-24 12:35:56 +0300

    feat: add redirect after check project

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { getSession } from '@/lib/auth';
+import prisma from '@/lib/prisma';
 import { redirect } from 'next/navigation';
 
 export default async function Dashboard() {
@@ -7,6 +8,23 @@ export default async function Dashboard() {
   if (!session?.user) {
     redirect('sign-in');
   }
+
+  // Проверяем, есть ли у пользователя проекты
+  const projects = await prisma.project.findMany({
+    where: {
+      createdByUser: {
+        id: session.user.id,
+      },
+    },
+  });
+
+  // Если проектов нет — редирект на онбординг
+  if (projects.length === 0) {
+    redirect('/dashboard/onboarding');
+  }
+
+  // Если проекты есть — редирект на первый проект
+  redirect(`/dashboard/projects/${projects[0].slug}`);
 
   return <section className='container mx-auto px-4'>Dashboard</section>;
 }
